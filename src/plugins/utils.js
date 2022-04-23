d6597375671821fe3ef4e1566e2f4e9879bb67c8
danhuaxiansheng <646042371@qq.com>
2022-04-23 20:05:43 +0800

    33

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -58,3 +58,34 @@ export function dateFormat(t) {
   }
   return str;
 }
+
+export function isMobileDevice() {
+  //判断当前设备是否为移动端
+  const ua = navigator.userAgent.toLowerCase();
+  const t1 =
+    /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(ua);
+  const t2 = !ua.match("iphone") && navigator.maxTouchPoints > 1;
+  return t1 || t2;
+}
+export function isMobilePhone() {
+  //判断当前设备是手机
+  const ua = navigator.userAgent.toLowerCase();
+  const t1 = /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(
+    ua
+  );
+  // const t2 = !ua.match("iphone") && navigator.maxTouchPoints > 1;
+  return t1;
+}
+export function isIOS() {
+  // const ua = navigator.userAgent;
+  // return !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+  var u = window.navigator.userAgent;
+  var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Linux") > -1; //g
+  return !isAndroid;
+}
+
+// 判断是否为移动端
+export function isBobile() {
+  let isTrue = isMobileDevice() || isMobilePhone();
+  return isTrue;
+}
--- vue.config.js
+++ vue.config.js
@@ -9,3 +9,23 @@ module.exports = defineConfig({
     });
   },
 });
+
+// "postcss": {
+//   "plugins": {
+//     "autoprefixer": {
+//       "overrideBrowserslist": [
+//         "Android 4.1",
+//         "iOS 7.1",
+//         "Chrome > 31",
+//         "ff > 31",
+//         "ie >= 8"
+//       ]
+//     },
+//     "postcss-pxtorem": {
+//       "rootValue": 85,
+//       "propList": [
+//         "*"
+//       ]
+//     }
+//   }
+// }
