336f3ca51afd683886b34df053e1a4fcd2e69d2e
danhuaxiansheng <646042371@qq.com>
2022-04-24 17:45:47 +0800

    000

--- postcss.config.js
+++ postcss.config.js
@@ -38,10 +38,34 @@
 //       ]
 //     },
 //     "postcss-pxtorem": {
-//       "rootValue": 64,
+//       "rootValue": 85,
 //       "propList": [
 //         "*"
 //       ]
 //     }
 //   }
 // }
+
+// const utils = require("./src/plugins/utils");
+// const isTrue = utils.isBobile();
+// let options = {};
+// if (isMobileDevice() && isMobilePhone()) {
+// options = {
+//   plugins: {
+//     autoprefixer: {
+//       overrideBrowserslist: [
+//         "Android 4.1",
+//         "iOS 7.1",
+//         // "Chrome > 31",
+//         // "ff > 31",
+//         // "ie >= 8",
+//       ],
+//     },
+//     "postcss-pxtorem": {
+//       rootValue: 85,
+//       propList: ["*"],
+//     },
+//   },
+// };
+// // }
+// module.exports = options;
--- src/main.js
+++ src/main.js
@@ -11,11 +11,11 @@ import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
 
 // import "lib-flexible";
-import { isBobile } from "@/plugins/utils";
-const isTrue = isBobile();
-if (isTrue === true) {
-  require("lib-flexible/flexible.js");
-}
+// import { isBobile } from "@/plugins/utils";
+// const isTrue = isBobile();
+// if (isTrue === true) {
+//   require("lib-flexible/flexible.js");
+// }
 const app = createApp(App);
 
 app.use(store).use(router).use(Antd).mount("#app");
