14092b2f5e2bf71559b727d7eed54a32313519d4
danhuaxiansheng <646042371@qq.com>
2022-05-08 12:03:45 +0800

    23

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ import "@/assets/css/base.css";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
 
-// import "lib-flexible";
-import { isMobile } from "@/plugins/utils";
-const isTrue = isMobile();
-if (isTrue === true) {
-  require("lib-flexible/flexible.js");
-}
+import "lib-flexible";
+// import { isMobile } from "@/plugins/utils";
+// const isTrue = isMobile();
+// if (isTrue === true) {
+//   require("lib-flexible/flexible.js");
+// }
 const app = createApp(App);
 
 app.use(store).use(router).use(Antd).mount("#app");
