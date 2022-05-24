e41e162eaeb3e0e22447659ee72f46e5fc9941b0
danhuaxiansheng <646042371@qq.com>
2022-05-24 16:43:21 +0800

    提交界面代码

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import "@/assets/css/base.css";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
 
+// import vue3videoPlay from "vue3-video-play"; // 引入组件
+// import "vue3-video-play/dist/style.css"; // 引入css
+
 const app = createApp(App);
+// 引入视频插件
+// app.use(vue3videoPlay);
 
 app.use(store).use(router).use(Antd).mount("#app");
--- src/views/video/modules/playing.vue
+++ src/views/video/modules/playing.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="video-result-wrap" :class="isMobileClass ? 'mobile' : ''">
-    <video
+    <!-- <video
       id="my-video"
       class="video-wrap vjs-default-skin vjs-big-play-centered"
       controls
@@ -8,35 +8,42 @@
       width="320"
       height="240"
     >
-      <source
-        src="https://json.nbjx.vip:4399/Cache/d458809227be860cda8a4ca4a7be65eb/00eah6mspqaDab8XZAviZAIddJeorZZ8d6y7dgYvKFca3GtfHPYGe37ajpfsvzoMwa2X4Md42vq_0gAHrDqnqNuWsEp7Fu04w8IjfQqC.m3u8"
-        type="application/x-mpegURL"
-      />
-    </video>
+      <source :src="m3u8Src" type="application/x-mpegURL" />
+    </video> -->
+
+    <!-- <video
+      ref="videoPlayer"
+      controls
+      :src="fileUrl"
+      :poster="posterUrl"
+      controlslist="nodownload"
+      @play="toPlays"
+      @timeupdate="updateTime"
+      @pause="toPause"
+      :class="[porel, fullwidth, mal, fla, mat, 'movie-show-video']"
+      style="width: 100%; height: 100%; object-fit: fill"
+    ></video>
+
+    <li @click="toPlay">播放/暂停</li>
+    <li @click="toForward">前进</li>
+    <li @click="toBack">倒退</li>
+    <li @click="addVoice">增加⾳量</li>
+    <li @click="decrease">减⼩⾳量</li> -->
+
+    <videoPlay v-bind="options" :poster="baseImg" />
   </div>
 </template>
 
 <script>
-import "video.js/dist/video-js.css";
-import "videojs-contrib-hls";
-import videojs from "video.js";
+import "vue3-video-play/dist/style.css";
+import { videoPlay } from "vue3-video-play";
 
 import { isMobile } from "@/plugins/utils";
 
 export default {
   name: "bookResult",
-  data() {
-    return {
-      // 数据列表
-      studyTime: {
-        currentTime: 0, // 当前已学时长
-        duration: 0, // 总时长
-      },
-      m3u8Src: "https://new.iskcd.com/20211218/u4JqnSI6/1100kb/hls/index.m3u8",
-    };
-  },
-  mounted() {
-    this.getVideo();
+  components: {
+    videoPlay,
   },
   computed: {
     // 是否为移动端
@@ -48,57 +55,40 @@ export default {
       return unescape(this.$route.query.url || "");
     },
   },
-  methods: {
-    // 初始化视频
-    getVideo() {
-      // 播放 this.play()
-      // 停止 – video没有stop方法，可以用pause 暂停获得同样的效果
-      // 暂停 this.pause()
-      // 销毁 this.dispose()
-      // 监听 this.on(‘click‘,fn)
-      // 触发事件this.trigger(‘dispose‘)
-      debugger;
-      videojs(
-        "my-video",
-        {
-          bigPlayButton: false,
-          textTrackDisplay: false,
-          posterImage: true,
-          errorDisplay: false,
-          controlBar: true,
-          // loop: true, // 视频播放结束后，是否循环播放
-          // muted: false, //是否静音
-          // poster: "", // 播放前显示的视频画面，播放开始之后自动移除。通常传入一个URL
-          // preload: "auto", //预加载‘ 自动
-
-          //  controls  //确定播放器是否具有用户可以与之交互的控件。没有控件，启动视频播放的唯一方法是使用autoplay属性或通过Player API。
-          //  autoplay: "muted", // //自动播放属性,muted:静音播放
-          //  preload="auto"  //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
-          //  poster="../assets/img/E0531.jpg">   //设置视频封面
-        },
-        function () {
-          this.play();
-
-          // How about an event listener?<br>  // 如何使用事件监听？
-          this.on("ended", function () {
-            videojs.log("‘播放结束了");
-          });
-        }
-      );
-    },
-
-    // 切换视频
-    checkVideo() {
-      var myPlayer = videojs("my-video");
-      myPlayer.src([
-        {
-          type: "application/x-mpegURL",
-          src: this.m3u8Src, //CCTV3频道
-        },
-      ]);
-      myPlayer.play();
-    },
+  data() {
+    return {
+      baseImg: require("@/assets/imgs/video/play_base.jpg"),
+      options: {
+        width: "800px", //播放器高度
+        height: "450px", //播放器高度
+        color: "#409eff", //主题色
+        title: "", //视频名称
+        type: "m3u8", //视频类型
+        src: "https://new.iskcd.com/20211218/u4JqnSI6/1100kb/hls/index.m3u8", //视频源
+        muted: false, //静音
+        webFullScreen: false,
+        speedRate: ["0.75", "1.0", "1.25", "1.5", "2.0"], //播放倍速
+        autoPlay: false, //自动播放
+        loop: false, //循环播放
+        mirror: false, //镜像画面
+        ligthOff: false, //关灯模式
+        volume: 0.3, //默认音量大小
+        control: true, //是否显示控制
+        controlBtns: [
+          "audioTrack",
+          "quality",
+          "speedRate",
+          "volume",
+          "setting",
+          "pip",
+          "pageFullScreen",
+          "fullScreen",
+        ], //显示所有按钮,
+      },
+    };
   },
+  mounted() {},
+  methods: {},
 };
 </script>
 <style scoped>
