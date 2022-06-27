9bdbc110d7ae1dae3283eb5233768143ebae1330
danhuaxiansheng <646042371@qq.com>
2022-06-27 21:30:52 +0800

    x

--- src/setting.js
+++ src/setting.js
@@ -25,3 +25,14 @@ module.exports = {
     // api: "http://111.229.92.181:3003/",
   },
 };
+
+// 漫画解析地址
+// http://www.766kan.com/list/index4.html
+
+// 盗版地址
+// https://www.iimanhua.cc/comic/28936/793577.html
+//https://www.iimanhua.cc/comic/45400/829094.html
+// 解析地址
+//http://www.nikucms.com/
+// 解析集合
+// http://jiexi.nikucms.com/
