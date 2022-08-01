8bf9385525f1ac36a446756741b7b7fe1395d25f
danhauxiansheng <646042371@qq.com>
2022-08-01 11:35:34 +0800

    【+】新增安全验证

--- src/plugins/safe.js
+++ src/plugins/safe.js
@@ -11,6 +11,29 @@ function showDebugger() {
     block();
   })();
 }
+// 清空html
+function clearHtml() {
+  (() => {
+    function block() {
+      if (
+        window.outerHeight - window.innerHeight > 200 ||
+        window.outerWidth - window.innerWidth > 200
+      ) {
+        document.body.innerHTML = "检测到非法调试,请关闭后刷新重试!";
+      }
+      setInterval(() => {
+        (function () {
+          return false;
+        }
+          ["constructor"]("debugger")
+          ["call"]());
+      }, 50);
+    }
+    try {
+      block();
+    } catch (err) {}
+  })();
+}
 
 // 防调试
 function devtools() {
@@ -104,4 +127,5 @@ function devtools() {
 module.exports = {
   showDebugger,
   devtools,
+  clearHtml,
 };
