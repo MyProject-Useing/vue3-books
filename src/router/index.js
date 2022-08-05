0a6a42b9fad56d1d88fccc7f010c660d19c162f7
danhauxiansheng <646042371@qq.com>
2022-08-05 15:19:45 +0800

    【*】更改首页信息

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
     path: "/",
     name: "home",
     component: () => import("@/components/layout.vue"),
-    redirect: "/bookIndex",
+    redirect: "/videoIndex",
     children: [
       {
         path: "/videoIndex",
